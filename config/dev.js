1bf0edad651ef043d06db3d04e1c1ce5d5ae6648
vipinsanthosh <58885397+vipinsanthosh@users.noreply.github.com>
2020-05-16 08:28:58 +0530

    Delete dev.js

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-// this file is used for development
-module.exports = {
-    MongoDB : 'mongodb+srv://bookRental:1234v5678@bookrental-plgan.mongodb.net/test?retryWrites=true&w=majority',
-    FBAppID: '271489820560834',
-    FBAppSECRET: '59c9c23531420f78db584b28a5e48f9d',
-    AWSAccessID: 'AKIA3CCZI2T327VLC7EW',
-    AWSSecretKey: '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'
-};
