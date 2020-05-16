455895830b9d99278998a10403972c2772d99259
vipinsanthosh <58885397+vipinsanthosh@users.noreply.github.com>
2020-05-16 08:22:00 +0530

    fourth commit

--- config/dev.js
+++ config/dev.js
@@ -3,6 +3,6 @@ module.exports = {
     MongoDB : 'mongodb+srv://bookRental:1234v5678@bookrental-plgan.mongodb.net/test?retryWrites=true&w=majority',
     FBAppID: '271489820560834',
     FBAppSECRET: '59c9c23531420f78db584b28a5e48f9d',
-    AWSAccessID: 'AKIA3CCZI2T3VAFZ7GOB',
-    AWSSecretKey: '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'
+    AWSAccessID: 'AKIA3CCZI2T327VLC7EW',
+    AWSSecretKey: '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'
 };
